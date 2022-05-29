b7d0b644b59c068ffed018411a93c4ad338d56c6
AdamNurthoriq <adamnur288@gmail.com>
2022-05-29 12:27:08 +0700

    first commit

---
+++ app/src/main/java/com/example/intent_eksplisit/TampilDataActivity.kt
@@ -0,0 +1,24 @@
+package com.example.intent_eksplisit
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class TampilDataActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tampil_data)
+
+        val tvData=findViewById<TextView>(R.id.tvData)
+        val nim = intent.getStringExtra("NIM")
+        val nama = intent.getStringExtra("Nama")
+
+        tvData.text= """ 
+           Data yang dimasukkan:
+           NIM: $nim
+           Nama:$nama
+        """.trimIndent()
+    }
+}
